9a75746961a75abebdbcdaca26ad592581fd5340
HoangDucBach <hoangbach0985@gmail.com>
2025-04-27 06:41:16 +0700

    fix: sdk

--- platform/src/sdk/constants.ts
+++ platform/src/sdk/constants.ts
@@ -1,8 +1,8 @@
 import { ChatiwalPackageConfig } from "./types";
 
 export const TESTNET_CHATIWAL_PACKAGE_CONFIG: ChatiwalPackageConfig = {
-    chatiwalId: "0x9ac8c60efedc8ef0890f4b7ce21f703ce0234b1b3449e8fa9ca2a6e70bea22fc",
-    registryObjectId: "0x20efe3925dddb7cb5b3ecf050e5b087baf2ef82e31c94353137fa584e971a128",
+    chatiwalId: "0x38aec0747e6f46aa43785fb0414f661cdb3420c914055ffc32f25322acf1682f",
+    registryObjectId: "0x1e02d0cbaa0d8cfdc5893161ed1ade25e21d10eb76bd553db66c130743f43e33",
 }
 export const MAINNET_CHATIWAL_PACKAGE_CONFIG: ChatiwalPackageConfig = {
     chatiwalId: "0x1",
