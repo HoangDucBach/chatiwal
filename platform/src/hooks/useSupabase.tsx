c3a54fdf6a46173bc2eeeb3a4565d5fca1d88231
lyhduc <lyhongduc.34@gmail.com>
2025-04-30 14:10:18 +0700

    Feat hook superMessage

--- platform/src/hooks/useSupabase.tsx
+++ platform/src/hooks/useSupabase.tsx
@@ -5,6 +5,8 @@ import { useEffect, useMemo } from "react";
 interface SupabaseActionsProps {
     getGroupMemberships: (user: string) => Promise<string[]>;
     addGroupMembership: (user: string, groupId: string) => Promise<void>;
+    getSuperMessages: (groupId: string) => Promise<string[]>;
+    addSuperMessage: (groupId: string, superMessageId: string) => Promise<void>;
 }
 
 export function useSupabase(): SupabaseActionsProps {
@@ -43,6 +45,34 @@ export function useSupabase(): SupabaseActionsProps {
                 }
 
                 return memberships.map((membership) => membership.group_id);
+            },
+
+            addSuperMessage: async (groupId: string, superMessageId: string) => {
+                const { error: insertError } = await supabase
+                    .from("group_super_messages")
+                    .upsert(
+                        { group_id: groupId, super_message_id: superMessageId },
+                        { onConflict: 'group_id,super_message_id', ignoreDuplicates: true }
+                    )
+                    .select();
+
+                if (insertError) {
+                    throw new Error("Failed to insert super message id: ", insertError);
+                    throw insertError;
+                }
+            },
+            
+            getSuperMessages: async (groupId: string) => {
+                const { data: superMessages } = await supabase
+                    .from("group_super_messages")
+                    .select("super_message_id")
+                    .eq("group_id", groupId);
+
+                if (!superMessages) {
+                    throw new Error("Failed to fetch super message id for group: ", groupId);
+                }
+
+                return superMessages.map((superMessage) => superMessage.super_message_id);
             },
         };
     }, [supabase]);
