8ea713dbf9f4bd82237ea98cc16cd38dfce00102
HoangDucBach <hoangbach0985@gmail.com>
2025-06-14 17:13:17 +0700

    fix: landing page

--- platform/src/app/(homepage)/page.tsx
+++ platform/src/app/(homepage)/page.tsx
@@ -1,14 +1,13 @@
 import { PageLayout } from "@/components/ui/layout";
 import HeroSection from "./_components/HeroSection";
 import { Effects } from "./_components/Effects";
-import { Center, VStack } from "@chakra-ui/react";
-import { Posters } from "./_components/Posters";
-import IntroSection from "./_components/IntroSection";
+import { VStack } from "@chakra-ui/react";
+import { SupportedBrands } from "./_components/SupportedBrands";
 
 export default function Home() {
   return (
     <PageLayout
-      h={"fit"}
+      h={"full"}
       w={"full"}
       mx={"auto"}
       flexDirection={"column"}
@@ -19,10 +18,10 @@ export default function Home() {
       <VStack
         w={"full"}
         maxW={["full", "full", "4xl", "6xl"]}
+        h={"full"}
       >
         <Effects />
         <HeroSection />
-        <Posters />
       </VStack>
     </PageLayout>
   );
