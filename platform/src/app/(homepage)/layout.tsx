c06d29f8564bcea3f5420cf9d4805978c31ee8c4
HoangDucBach <hoangbach0985@gmail.com>
2025-06-14 21:05:55 +0700

    fix: snapshot

--- platform/src/app/(homepage)/layout.tsx
+++ platform/src/app/(homepage)/layout.tsx
@@ -1,8 +1,5 @@
-import { Footer } from "./_components/Footer";
 import { Header } from "./_components/Header";
 import { LayoutLayout } from "@/components/ui/layout";
-import SpaceSection from "./_components/SpaceSection";
-import { SmoothScrollLayout } from "./_components/SmoothScrollLayout";
 
 export default function Layout({
     children,
